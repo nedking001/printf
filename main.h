a667f44915ea6fb1a56900e81d40e52774a3f581
Chinedum <chinedume77@gmail.com>
2024-07-30 17:49:50 -0700

    Updated main.h

--- main.h
+++ main.h
@@ -71,6 +71,10 @@ char buffer[],
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
